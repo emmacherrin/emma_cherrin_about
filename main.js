4038dce82c25b364df8f01610a067e05f8ec8d35
emmacherrin <emmacherrin@gmail.com>
2023-11-25 00:45:50 -0500

    Z pos adjustments for bridgify, duplicate res.
    Resolving duplicate education section, more z position adjustments for bridgify

--- main.js
+++ main.js
@@ -124,7 +124,7 @@ var teaching_img = new THREE.Mesh(
 ); 
 
 // Position the image near the University of Michigan section
-teaching_img.position.z = 35;
+teaching_img.position.z = 36;
 teaching_img.position.x = -3;
 teaching_img.rotation.y = Math.PI / 4; // Adjust the angle as needed
 scene.add(teaching_img);
@@ -136,7 +136,7 @@ var bridgify = new THREE.Mesh(
 );
 
 // Position the image near the University of Michigan section
-bridgify.position.z = 40;
+bridgify.position.z = 43;
 bridgify.position.x = -3;
 bridgify.rotation.y = Math.PI / 4; // Adjust the angle as needed
 
